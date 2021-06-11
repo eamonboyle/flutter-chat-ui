d2a6312cbcd85dd6448ca415bc4589674c73ebbe
Eamon Boyle <blaowskate@hotmail.com>
2021-06-11 10:33:41 +0100

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:chat_ui/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        accentColor: Color(0xFFFEF9EB)
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
